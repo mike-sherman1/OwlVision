2ae395143e9aa6eb8ab7766fc677e76acd644296
astone2014 <astone2014@fau.edu>
2016-12-07 22:51:25 -0500

    fixed header spacing

--- src/app/models/issue.js
+++ src/app/models/issue.js
@@ -2,13 +2,13 @@
 var Issue = (function () {
     function Issue(issueObj) {
         this.title = issueObj && issueObj.title || '';
+        this.location = issueObj && issueObj.location || '';
         this.author = issueObj && issueObj.author || '';
         this.name = issueObj && issueObj.name || '';
-        this.email = issueObj && issueObj.email || '';
+        this.description = issueObj && issueObj.description || '';
+        //this.email = issueObj && issueObj.email || '';
         this.priority = issueObj && issueObj.priority || '';
         this.status = issueObj && issueObj.status || null;
-        this.description = issueObj && issueObj.description || '';
-        this.location = issueObj && issueObj.location || '';
         this.picture = issueObj && issueObj.picture || '';
         this.picture = issueObj && issueObj.picture || '';
         this.isAnonymous = issueObj && issueObj.isAnonymous || false;
